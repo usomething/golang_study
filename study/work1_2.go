840b017040a3e277f23f51d598a6bf51decaad48
kk <usomething@gmail.com>
2022-02-16 21:31:10 +0800

    完毕

--- study/work1_2.go
+++ study/work1_2.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 
 	chanInt := make(chan int, 10)
+
+	go func() {
+		tickerSend := time.NewTicker(1000 * time.Millisecond)
+		for i := 0; ; i++ {
+			<-tickerSend.C
+			chanInt <- i
+		}
+	}()
+
+	tickerRecv := time.NewTicker(1 * time.Second)
+	for {
+		<-tickerRecv.C
+		ret := <-chanInt
+		fmt.Printf("recv : %d\n", ret)
+	}
 
 }
